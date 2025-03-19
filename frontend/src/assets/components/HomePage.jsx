23bec764b3d71d3aed86797d7f437afb7e243e6d
Tomislav Bratić <ticbra1337@gmail.com>
2025-03-19 21:12:28 +0100

    added toolbar and homepage,added pictures and logos

---
+++ frontend/src/assets/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import Toolbar from "./Toolbar"
+
+const HomePage = ({toggleDarkMode}) => {
+    return (
+        <div>
+            <Toolbar toggleDarkMode={toggleDarkMode}/>
+            <main className="p-8">
+                <h2 className="text-3x1 font-bold mb-4 text-gray-400"> Welcome to Goloko</h2>
+                <p className="text-gray-700 dark:text-gray-300">Here you can add your events</p>
+            </main>
+        </div>
+    );
+};
+
+export default HomePage;
